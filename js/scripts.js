576c6fac7ccd532b295c7eaee46aa9761115f017
tglass3 <120339411+tglass3@users.noreply.github.com>
2023-05-16 16:46:47 +0000

    doing shit

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,24 @@
 // My Scripts
-console.log('Hello World!');
+console.log('👍 JS Connected');
+
+const gradient = ['bg0', 'bg1', 'bg2', 'bg3', 'bg4', 'bg5', 'bg6', 'bg7', 'bg8'];
+const limit = gradient.length;
+const randNum = Math.floor(Math.random()*limit);
+
+const body = document.querySelector('body');
+
+body.classList.add(gradient[randNum]);
+
+
+
+// get all the card
+document.querySelectorAll('.card').forEach( item => {
+    
+    // for each item i.e. card, do this
+    item.addEventListener('click', event => {
+      
+      // toggle class on card
+      item.classList.toggle('flipped');
+    })
+  
+})
